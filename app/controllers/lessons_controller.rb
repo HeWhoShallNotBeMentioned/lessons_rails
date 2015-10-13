0f89191289363c31bd16931c2b48be2d3a0817e1
Janice Laset-Parkerson & chris underwood <student@epicodus.com>
2015-10-13 14:35:14 -0700

    allow user to add new lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,4 +8,23 @@ class LessonsController < ApplicationController
     @lesson = Lesson.find(params[:id])
     render :show
   end
+
+  def new
+    @lesson = Lesson.new
+    render :new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(:name)
+  end
 end
